b9f9da04e4f41cca05b6255e6007cd80b0fdf0b9
Minninnewah <40040609+Minninnewah@users.noreply.github.com>
2022-11-11 14:06:57 +0100

    Update setup_environment_worker.sh

--- testbed/setup_environment_worker.sh
+++ testbed/setup_environment_worker.sh
@@ -1,14 +1,15 @@
+echo "Install CRIU"
 cd $HOME/tmp
-#git clone https://github.com/SSU-DCN/podmigration-operator.git
 cd podmigration-operator
 tar -vxf criu-3.14.tar.bz2
 cd criu-3.14
-sudo apt-get install protobuf-c-compiler libprotobuf-c0-dev protobuf-compiler \
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get install protobuf-c-compiler libprotobuf-c0-dev protobuf-compiler \
 libprotobuf-dev:amd64 gcc build-essential bsdmainutils python git-core \
 asciidoc make htop git curl supervisor cgroup-lite libapparmor-dev \
 libseccomp-dev libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler \
 protobuf-compiler python-protobuf libnl-3-dev libcap-dev libaio-dev \
-apparmor libnet1-dev libnl-genl-3-dev libnl-route-3-dev libnfnetlink-dev pkg-config
+apparmor libnet1-dev libnl-genl-3-dev libnl-route-3-dev libnfnetlink-dev pkg-config -y
 
 make clean
 make
@@ -19,3 +20,9 @@ sudo mkdir /etc/criu
 sudo touch /etc/criu/runc.conf
 echo 'tcp-established
 tcp-close' | sudo tee /etc/criu/runc.conf >/dev/null
+
+
+echo "Configure NFS share folder"
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install nfs-common -y
+sudo mount -t nfs -o nfsvers=3 10.10.3.124:/var/lib/kubelet/migration /var/lib/kubelet/migration
