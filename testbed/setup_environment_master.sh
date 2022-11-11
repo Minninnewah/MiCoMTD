c933fbe34005c878e04dccbe23b645a48797e9ac
Minninnewah <40040609+Minninnewah@users.noreply.github.com>
2022-11-11 14:08:52 +0100

    Update setup_environment_master.sh

--- testbed/setup_environment_master.sh
+++ testbed/setup_environment_master.sh
@@ -1,7 +1,19 @@
+echo "Setup kubeadm"
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+echo "Apply flannel CNI add-on network"
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl get pods -n kube-system
+
+
 sudo mkdir /var/lib/kubelet/migration
 sudo apt-get update
-sudo apt-get install nfs-kernel-server -y
-echo '/var/lib/kubelet/migration/  192.168.10.0/24(rw,sync,no_subtree_check)' | sudo tee /etc/exports >/dev/null
+sudo DEBIAN_FRONTEND=noninteractive apt-get install nfs-kernel-server -y
+echo '/var/lib/kubelet/migration/  10.10.0.0/16(rw,sync,no_subtree_check)' | sudo tee /etc/exports >/dev/null
 sudo exportfs -arvf
 sudo systemctl start nfs-kernel-server
 sudo systemctl enable nfs-kernel-server
