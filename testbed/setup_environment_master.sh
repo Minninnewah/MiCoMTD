2c2883314bc1e2bf3d65308f82bc821d85b97c2d
Minninnewah <40040609+Minninnewah@users.noreply.github.com>
2022-11-14 16:18:32 +0100

    Update setup_environment_master.sh

--- testbed/setup_environment_master.sh
+++ testbed/setup_environment_master.sh
@@ -1,3 +1,21 @@
+#-r param is for the local ip address range of the worker nodes like 10.0.0.0/16
+if [ $# -ne 2 ]; then
+   echo "Only one flag allowed";
+   exit;
+fi
+
+if [ "$1" != "-r" ]; then
+  echo "Only -h flag supported"
+  exit;
+fi
+
+while getopts r: flag
+do
+        case "${flag}" in
+                r) range=${OPTARG};;
+        esac
+done
+
 echo "Setup kubeadm"
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 
@@ -13,7 +31,7 @@ kubectl get pods -n kube-system
 sudo mkdir /var/lib/kubelet/migration
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install nfs-kernel-server -y
-echo '/var/lib/kubelet/migration/  10.0.0.0/16(rw,sync,no_subtree_check)' | sudo tee /etc/exports >/dev/null
+echo '/var/lib/kubelet/migration/  $range(rw,sync,no_subtree_check)' | sudo tee /etc/exports >/dev/null
 sudo exportfs -arvf
 sudo systemctl start nfs-kernel-server
 sudo systemctl enable nfs-kernel-server
