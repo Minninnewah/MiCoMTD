ca0a1d4d3b50f648b6ffa108b330b76b8e367b04
Minninnewah <40040609+Minninnewah@users.noreply.github.com>
2022-11-14 16:27:55 +0100

    Update setup_environment_complete.sh

--- testbed/setup_environment_complete.sh
+++ testbed/setup_environment_complete.sh
@@ -1,9 +1,11 @@
 read -p 'Master or worker? [m/w]: ' role
 if [  $role == "m"  ]; then
         echo "Set up Master node"
+        read -p 'Local ip address range of the worker nodes to set up the nfs server (like 10.0.0.0/16): ' workerRange
 elif [ $role == "w" ]; then
         echo "Set up Worker node"
         read -p 'Enter join command from master node: ' joinCommand
+        read -p 'Enter local ip of master node: ' masterIP
 else
         echo "Abort, Inacceptable selection"
 fi
@@ -13,11 +15,11 @@ wget -O - https://raw.githubusercontent.com/Minninnewah/MiCoMTD/main/testbed/set
 
 if [  $role == "m"  ]; then
         echo "Master specific setup"
-        wget -O - https://raw.githubusercontent.com/Minninnewah/MiCoMTD/main/testbed/setup_environment_master.sh | bash
+        wget -O - https://raw.githubusercontent.com/Minninnewah/MiCoMTD/main/testbed/setup_environment_master.sh | bash /dev/stdin -r $workerRange
 elif [ $role == "w" ]; then
         echo "Worker specific setup"
         echo "Joint to master noder"
         $joinCommand
-        wget -O - https://raw.githubusercontent.com/Minninnewah/MiCoMTD/main/testbed/setup_environment_worker.sh | bash
+        wget -O - https://raw.githubusercontent.com/Minninnewah/MiCoMTD/main/testbed/setup_environment_worker.sh | bash /dev/stdin -h $masterIP
 fi
         
