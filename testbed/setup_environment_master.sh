f2173efd3a862ec45fc6ad0a12ea96a128a8d8df
Minninnewah <40040609+Minninnewah@users.noreply.github.com>
2022-11-14 09:09:49 +0100

    Update setup_environment_master.sh

--- testbed/setup_environment_master.sh
+++ testbed/setup_environment_master.sh
@@ -13,9 +13,10 @@ kubectl get pods -n kube-system
 sudo mkdir /var/lib/kubelet/migration
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install nfs-kernel-server -y
-echo '/var/lib/kubelet/migration/  10.10.0.0/16(rw,sync,no_subtree_check)' | sudo tee /etc/exports >/dev/null
+echo '/var/lib/kubelet/migration/  10.0.0.0/16(rw,sync,no_subtree_check)' | sudo tee /etc/exports >/dev/null
 sudo exportfs -arvf
 sudo systemctl start nfs-kernel-server
 sudo systemctl enable nfs-kernel-server
 sudo systemctl status nfs-kernel-server
+sudo chmod 777 /var/lib/kubelet/
 sudo chmod 777 /var/lib/kubelet/migration
