3cddd232b42e48a080fae9a55eb534b43d67d386
heebzen1 <heebzen1@students.zhaw.ch>
2023-02-12 18:44:06 +0100

    Renamed MTD_controller to MTD_coordinator

--- src/MTD_coordinator/test.py
+++ src/MTD_coordinator/test.py
@@ -2,6 +2,7 @@ from api.topofuzzer_handler import TopoFuzzerHandler
 import environment
 import requests
 from action_executors.reinstantiation_executor import ReinstantiationExecutor
+import time
 
 if __name__ == "__main__":
     #topoFuzzer_handler = TopoFuzzerHandler(environment.TopoFuzzer_IP, environment.TopoFuzzer_PORT)
@@ -10,6 +11,9 @@ if __name__ == "__main__":
     #topoFuzzer_handler.expose_service("160.85.253.118", "160.85.253.79")
     #results = topoFuzzer_handler.get_all_entries()
     #print(results)
+    while True:
+        time.sleep(2 - time.time() % 2)
+        print("Test")
 
-    executor = ReinstantiationExecutor()
-    executor.execute()
+    #executor = ReinstantiationExecutor()
+    #executor.execute()
